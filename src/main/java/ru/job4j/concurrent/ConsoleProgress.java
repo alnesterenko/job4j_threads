942954d97dd66670c9ff559a79a786b068682d9c
alnesterenko <alzpupkin@gmail.com>
2024-12-17 15:58:25 +0200

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/' };
+        int i = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                i = i >= process.length - 1 ? 0 : ++i;
+                System.out.print("\r Loading ... " + process[i]);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+}
