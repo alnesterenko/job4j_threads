f0cf75bea3e77b2097ee78d541f508b2e3b57888
alnesterenko <alzpupkin@gmail.com>
2024-12-19 10:47:32 +0200

    9. ThreadLocal [#504912]. Пример. Эксперементы.

--- src/main/java/ru/job4j/threads/threadlocal/FirstThread.java
+++ src/main/java/ru/job4j/threads/threadlocal/FirstThread.java
@@ -5,5 +5,9 @@ public class FirstThread extends Thread {
     public void run() {
         ThreadLocalDemo.threadLocal.set("Это поток 1.");
         System.out.println(ThreadLocalDemo.threadLocal.get());
+        ThreadLocalDemo.initialList();
+        ThreadLocalDemo.printList();
+        ThreadLocalDemo.changeList();
+        ThreadLocalDemo.printList();
     }
 }
--- src/main/java/ru/job4j/threads/threadlocal/ThreadLocalDemo.java
+++ src/main/java/ru/job4j/threads/threadlocal/ThreadLocalDemo.java
@@ -1,13 +1,40 @@
 package ru.job4j.threads.threadlocal;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ThreadLocalDemo {
     public static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+    public static ThreadLocal<List<String>> threadLocalStringList = new ThreadLocal<>();
+
+    public static void initialList() {
+        threadLocalStringList.set(Arrays.asList("one", "two", "three"));
+    }
+
+    public static void printList() {
+        threadLocalStringList.get().forEach(System.out::println);
+    }
+
+    public static void changeList() {
+        threadLocalStringList.set(threadLocalStringList.get().stream()
+                .map(elem -> elem + " Из нити: " + Thread.currentThread().getName())
+                .toList());
+    }
 
     public static void main(String[] args) throws InterruptedException {
         Thread first = new FirstThread();
         Thread second = new SecondThread();
+        ThreadLocalDemo.initialList();
         threadLocal.set("Это поток main.");
         System.out.println(threadLocal.get());
+        ThreadLocalDemo.printList();
+        ThreadLocalDemo.changeList();
+        ThreadLocalDemo.printList();
+        first.setPriority(10);
+        System.out.println("Установленные приоритеты: "
+                + Thread.currentThread().getName() + " = " + Thread.currentThread().getPriority()
+                + ", " + first.getName() + " = " + first.getPriority()
+                + ", " + second.getName() + " = " + second.getPriority());
         first.start();
         second.start();
         first.join();
