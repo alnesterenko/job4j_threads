1dcc6463bbf38e4dd99eb070b75aadd57169afc3
alnesterenko <alzpupkin@gmail.com>
2025-01-16 06:45:53 +0200

    3. Immutable объекты [#267918]. Стало.

--- src/main/java/ru/job4j/linked/Node.java
+++ src/main/java/ru/job4j/linked/Node.java
@@ -1,22 +1,26 @@
 package ru.job4j.linked;
 
+/**
+ * Класс Node превращён из mutable в immutable. Это достигнуто благодаря:
+ * 1) Поля класса сделаны final.
+ * 2) Убраны сеттеры и вместо них добавлен конструктор, принимающий сразу оба значения для своих полей.
+ *      То есть, значения полей указываются один раз и больше не меняются.
+ * @param <T>
+ */
 public class Node<T> {
-    private Node<T> next;
-    private T value;
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
 
     public Node<T> getNext() {
         return next;
     }
 
-    public void setNext(Node<T> next) {
-        this.next = next;
-    }
-
     public T getValue() {
         return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
     }
 }
