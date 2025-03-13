ced3febcc9953220f980019d71ec24cb341d64bd
alnesterenko <alzpupkin@gmail.com>
2025-03-13 17:54:35 +0200

    1. Неблокирующий кеш [#4741]. Стало.

--- src/main/java/ru/job4j/concurrent/cache/Cache.java
+++ src/main/java/ru/job4j/concurrent/cache/Cache.java
@@ -9,17 +9,23 @@ import java.util.stream.Stream;
 public class Cache {
     private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
 
-    public boolean add(Base model) throws OptimisticException {
+    public boolean add(Base model) {
         return memory.putIfAbsent(model.id(), model) == null;
     }
 
     public boolean update(Base model) throws OptimisticException {
-        /* TODO impl */
-        return false;
+        return memory.computeIfPresent(model.id(),
+                (id, oldModel) -> {
+                    if (oldModel.version() != model.version()) {
+                        throw new OptimisticException("Versions are not equal");
+                    }
+                    return new Base(id, model.name(), model.version() + 1);
+                }
+                ) != null;
     }
 
     public void delete(int id) {
-        /* TODO impl */
+        memory.remove(id);
     }
 
     public Optional<Base> findById(int id) {
--- src/test/java/ru/job4j/concurrent/cache/CacheTest.java
+++ src/test/java/ru/job4j/concurrent/cache/CacheTest.java
@@ -46,5 +46,20 @@ class CacheTest {
                 .isInstanceOf(OptimisticException.class);
     }
 
-    /* TODO добавить тест на многократное ПРАВИЛЬНОЕ обновление */
+    /* Да, при многопоточке, следующий тест не сработает. */
+    @Test
+    public void whenMultiUpdateThenOk() throws OptimisticException {
+        var firstBase = new Base(1,  "Base", 1);
+        var cache = new Cache();
+        cache.add(firstBase);
+        var currentBase = cache.findById(1).get();
+        cache.update(new Base(currentBase.id(), "secondBase", currentBase.version()));
+        currentBase = cache.findById(1).get();
+        cache.update(new Base(currentBase.id(), "thirdBase", currentBase.version()));
+        currentBase = cache.findById(1).get();
+        cache.update(new Base(currentBase.id(), "fourthBase", currentBase.version()));
+        currentBase = cache.findById(1).get();
+        assertThat(currentBase.version()).isEqualTo(4);
+        assertThat(currentBase.name()).isEqualTo("fourthBase");
+    }
 }
